10eba815981abf35e761016585cd7eff998ba758
reinalddy <reinalddyaldy183@gmail.com>
2025-11-29 17:41:03 +0700

    fix: login proses

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -4,29 +4,53 @@ import * as userService from './user.service.js';
 export const register = async (req, res) => {
     try {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        if (!errors.isEmpty()) return res.status(400).json({
+            code: 400, 
+            message: 'Validation error', 
+            data: errors.array() 
+        });
 
         const { email, password, name } = req.body;
         const user = await userService.registerUser(email, password, name);
 
-        res.status(201).json({ message: 'User registered', user });
+        res.status(200).json({
+            code:200, 
+            message: 'User registered', 
+            data: user
+        });
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(500).json({code: 500, message: err.message });
     }
 };
 
 export const login = async (req, res) => {
     try {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        if (!errors.isEmpty()) return res.status(400).json({ 
+            code: 400,
+            message: 'Validation error',
+            data: errors.array() 
+        });
 
         const { email, password } = req.body;
         const response = await userService.loginUser(email, password);
-        if(response.status === false) return res.status(401).json({ code: 401, message: response.message, token: null });
 
-        res.status(200).json({ message: 'Login successful', token: response.token });
+        if(response.status === false) return res.status(401).json({
+            code: 401, 
+            message: response.message, 
+            token: null 
+        });
+
+        res.status(200).json({
+            code: 200, 
+            message: 'Login successful', 
+            token: response.token 
+        });
     } catch (err) {
-        res.status(401).json({ message: err.message });
+        res.status(500).json({
+                code: 500,
+                message: err.message
+            });
     }
 };
 
